18409968215afa786549b9b8bd584d2366b597a2
William Buchwalter <wbuchwalter@gmail.com>
2025-06-25 10:55:27 +0200

    gemma

--- gpt/evaluator.py
+++ gpt/evaluator.py
@@ -248,15 +248,14 @@ def simple_evaluate(
         run_task_tests(task_list=tasks)
 
     # === Painter ===
-    if "Llama" in model_args or 'llama' in model_args:
+    if "Llama" in model_args or "llama" in model_args:
         from lm_eval.modify_model import modify_llama
+
         lm = modify_llama(lm, args)
-    elif "Mistral" in model_args:
-        from lm_eval.modify_model import modify_mistral
-        lm = modify_mistral(lm, args)
-    elif "neox" in model_args or "pythia" in model_args:
-        from lm_eval.modify_model import modify_neox
-        lm = modify_neox(lm, args)
+    elif "Gemma" in model_args:
+        from lm_eval.modify_model import modify_gemma
+
+        lm = modify_gemma(lm, args)
     else:
         raise ValueError("Only llama, mistral and neox models are supported")
 
@@ -533,16 +532,16 @@ def evaluate(
                     ]
 
                     # compute group's pooled metric and stderr
-                    results[group][
-                        metric
-                    ] = lm_eval.api.metrics.aggregate_subtask_metrics(metrics, sizes)
+                    results[group][metric] = (
+                        lm_eval.api.metrics.aggregate_subtask_metrics(metrics, sizes)
+                    )
                     # TODO: calculate grouped metric using aggregation fn
                     if "N/A" in stderrs:
                         results[group][stderr] = "N/A"
                     else:
-                        results[group][
-                            stderr
-                        ] = lm_eval.api.metrics.pooled_sample_stderr(stderrs, sizes)
+                        results[group][stderr] = (
+                            lm_eval.api.metrics.pooled_sample_stderr(stderrs, sizes)
+                        )
                         # TODO: allow GroupConfigs to choose which variance formula is used, for back-compatibility
                         # To use the old (likely incorrect) variance formula, comment out the above and uncomment this line:
                         # results[group][stderr] = lm_eval.api.metrics.combined_sample_stderr(stderrs, sizes, metrics=metrics)
--- gpt/modify_model.py
+++ gpt/modify_model.py
@@ -1,16 +1,36 @@
 import types
 from lm_eval.modeling_llama import LlamaAttention, LlamaDecoderLayer, LlamaCustomModel
+from lm_eval.modeling_gemma import CuGemma3ForCausalLM, Gemma3DecoderLayer
+
+
+def modify_gemma(lm, args=None):
+    print(f"modifying: {lm.model.model.__class__.__name__}")
+    lm.model.model.forward = types.MethodType(
+        CuGemma3ForCausalLM.forward, lm.model.model
+    )
+    for i in range(len(lm.model.model.layers)):
+        lm.model.model.layers[i].forward = types.MethodType(
+            Gemma3DecoderLayer.forward, lm.model.model.layers[i]
+        )
+        lm.model.model.layers[i].repeat_mlp = 1
+
+    if args.method == "smart":
+        lm.model.model.layers[8].repeat_mlp = 2
 
 
 def modify_llama(lm, args=None):
     # Add new attribute to llama model
-    lm.model.model.set_method(args.method)
     # lm.model.model.start_layer = args.start_layer
     # lm.model.model.repeat_time_or_seed = args.repeat_time_or_seed
     # lm.model.model.hidden_state_folder_path = args.hidden_state_folder_path
 
     # Modify the forward function into run different layer
+    lm.model.model.set_method = types.MethodType(
+        LlamaCustomModel.set_method, lm.model.model
+    )
     lm.model.model.forward = types.MethodType(LlamaCustomModel.forward, lm.model.model)
+    lm.model.model.set_method(args.method)
+
     # lm.model.model._pass_through_layer = types.MethodType(
     #     LlamaModel._pass_through_layer, lm.model.model
     # )
